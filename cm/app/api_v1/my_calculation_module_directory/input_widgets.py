4154ba1a33dc9fee5d1c90408f2551f35f6cf5fd
Jeton <jeton.hasani@student.tuwien.ac.at>
2019-07-29 12:04:21 +0200

    convert all values in input values INPUTS_CALCULATION_MODULE to strings

--- cm/app/api_v1/my_calculation_module_directory/input_widgets.py
+++ cm/app/api_v1/my_calculation_module_directory/input_widgets.py
@@ -6,11 +6,19 @@ path_inputs_parameter = os.path.join(path2data,"INPUTS_CALCULATION_MODULE.xlsx")
 
 
 def load_input_widgets(CM_ID,path=path_inputs_parameter):
-    df = pd.read_excel(path)
+    df = pd.read_excel(path).astype(str)
     df["cm_id"] = CM_ID
-    return df.to_dict(orient='records'),None 
-
+    out = df.to_dict(orient='records')
+    
+    for i,data in enumerate(out):
+        if data["input_type"] == "select":
+            out[i]["input_value"]= data["input_value"].replace("[","").replace("]","").replace("'","").replace('"','').split(",")    
+    return out,None
 
 
 if __name__ == "__main__":
     print('Main: input_widgets.py')
+    x,_= load_input_widgets("XX")
+#    import pprint
+#    pp = pprint.PrettyPrinter(indent=4)
+#    pp.pprint(x)
